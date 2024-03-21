92726f9a30bb2619b878dbeae805e135c268ffb4
aledezzy <alessandrodezuani@outlook.it>
2024-03-21 10:55:46 +0100

    mvc start

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+//site name
+define('tic', 'your-site-name');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'scuola');
+define('DB_PASS', 'pierino');
+define('DB_NAME', 'DBMuseo');
+
+?>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+// Load Config
+require_once '../config/config.php';
+
+// Autoload Core Libraries
